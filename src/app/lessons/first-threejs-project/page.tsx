7f6271d53e6f375d1e0a21916159a365d48ebbd6
zuzokim <zuzokim@gmail.com>
2024-02-06 14:35:24 +0900

    first three js project

---
+++ src/app/lessons/first-threejs-project/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import * as THREE from "three";
+
+function Page() {
+  const el = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (!el.current) {
+      return;
+    }
+    el.current.innerHTML = "HELLO 3";
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    el.current.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ffee });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+
+    function animate() {
+      requestAnimationFrame(animate);
+
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderer.render(scene, camera);
+    }
+
+    animate();
+  }, []);
+  return <div ref={el}></div>;
+}
+
+export default Page;
