6285e8540483d31b2535a2387133c9c72e64640a
zuzokim <zuzokim@gmail.com>
2024-02-19 23:41:36 +0900

    animate at same speed using deltatime

--- src/app/lessons/animation/page.tsx
+++ src/app/lessons/animation/page.tsx
@@ -26,7 +26,7 @@ function Page() {
     el.current.appendChild(renderer.domElement);
 
     const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ffee });
+    const material = new THREE.MeshBasicMaterial({ color: 0x0077dd });
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
 
@@ -39,6 +39,10 @@ function Page() {
 
     renderer.render(scene, camera);
 
+    //previous time
+    let time = Date.now();
+    
+
     function animate() {
       /**call it on the next frame!
        * if it's on a high frame rate computer screen,
@@ -46,9 +50,14 @@ function Page() {
        */
       window.requestAnimationFrame(animate);
 
-      cube.position.x += 0.005;
-      cube.rotation.x += 0.005;
-      cube.rotation.y += 0.005;
+      const currentTime = Date.now();
+      /**deltaTime would be shorter if the frame rate is high.*/
+      const deltaTime = currentTime - time;
+      time = currentTime;
+
+      /**animate at the same speed regardless of the fps */
+      cube.rotation.x += 0.005 * deltaTime;
+      cube.rotation.y += 0.03 * deltaTime;
 
       renderer.render(scene, camera);
     }
