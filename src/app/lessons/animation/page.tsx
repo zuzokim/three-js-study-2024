5ee0048b81fc9ccdede5986abf2ddc4de7d4a06d
zuzokim <zuzokim@gmail.com>
2024-02-19 23:31:38 +0900

    animate using requestAnimationFrame

---
+++ src/app/lessons/animation/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import * as THREE from "three";
+import HomeButton from "../../components/HomeButton";
+import styles from "../../page.module.css";
+
+function Page() {
+  const el = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (!el.current) {
+      return;
+    }
+    el.current.innerHTML = "";
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    el.current.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ffee });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    const axedHelper = new THREE.AxesHelper();
+    scene.add(axedHelper);
+
+    camera.position.z = 5;
+    camera.position.y = 1;
+    camera.position.x = 1;
+
+    renderer.render(scene, camera);
+
+    function animate() {
+      /**call it on the next frame!
+       * if it's on a high frame rate computer screen,
+       * it will be called more frequently.
+       */
+      window.requestAnimationFrame(animate);
+
+      cube.position.x += 0.005;
+      cube.rotation.x += 0.005;
+      cube.rotation.y += 0.005;
+
+      renderer.render(scene, camera);
+    }
+
+    animate();
+  }, []);
+  return (
+    <div className={styles.page}>
+      <HomeButton />
+      <div ref={el}></div>
+    </div>
+  );
+}
+
+export default Page;
