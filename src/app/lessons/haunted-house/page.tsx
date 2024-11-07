7e1b0c56bf8b9b63bfd5226ce05c815d3e669958
zuzokim <zuzokim@gmail.com>
2024-11-07 23:00:44 +0900

    snowman with particles

--- src/app/lessons/haunted-house/page.tsx
+++ src/app/lessons/haunted-house/page.tsx
@@ -8,6 +8,9 @@ import HomeButton from "@/app/components/HomeButton";
 import PageTitle from "@/app/components/PageTitle";
 import { Timer } from "three/addons/misc/Timer.js";
 import { Sky } from "three/examples/jsm/objects/Sky.js";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+import { text } from "stream/consumers";
+import { loadTexture } from "@/app/utils/textureLoader";
 
 function Page() {
   const el = useRef<HTMLCanvasElement>(null);
@@ -68,7 +71,7 @@ function Page() {
       // Walls
       const walls = new THREE.Mesh(
         new THREE.BoxGeometry(4, 2.5, 4),
-        new THREE.MeshStandardMaterial({  color: "#072d2d",})
+        new THREE.MeshStandardMaterial({ color: "#d53fe6" })
       );
       walls.position.y = 1.25; //2.5의 절반
       house.add(walls);
@@ -77,7 +80,7 @@ function Page() {
       const roof = new THREE.Mesh(
         new THREE.ConeGeometry(3.5, 1.5, 4),
         new THREE.MeshStandardMaterial({
-          color: "red",
+          color: "#390ceb",
         })
       );
       roof.position.y = 2.5 + 0.75; //walls의 y위치 + walls의 절반
@@ -87,7 +90,7 @@ function Page() {
       // Door
       const door = new THREE.Mesh(
         new THREE.PlaneGeometry(2.2, 2.2),
-        new THREE.MeshStandardMaterial({ color: "red" })
+        new THREE.MeshStandardMaterial({ color: "#e0a21b" })
       );
       door.position.y = 1;
       door.position.z = 2 + 0.001; //z-fighting 방지
@@ -127,26 +130,105 @@ function Page() {
 
       //Snowman
 
-      const snowmanGroup = new THREE.Group();
-      scene.add(snowmanGroup);
-      const snowmanGeometry = new THREE.SphereGeometry(1, 16, 16);
-      const snowmanMaterial = new THREE.MeshStandardMaterial({
-        color: "white",
-      });
+      const loader = new OBJLoader();
 
-      const snowman = new THREE.Mesh(snowmanGeometry, snowmanMaterial);
-      snowman.scale.set(0.7, 0.7, 0.7);
-      snowman.position.set(2.8, 0.3, 4.5);
+      const snowmanModel = new URL(
+        "./models/snowman_final.obj",
+        import.meta.url
+      );
 
-      const snowmanhead = new THREE.Mesh(snowmanGeometry, snowmanMaterial);
-      snowmanhead.scale.set(0.4, 0.4, 0.4);
-      snowmanhead.position.set(2.8, 1.2, 4.5);
+      const carrot = new URL("./textures/snowman/Carrot.jpg", import.meta.url);
+      const carrotTexture = textureLoader.load(carrot.pathname);
 
-      snowmanGroup.add(snowman, snowmanhead);
+      const blackFelt = new URL(
+        "./textures/snowman/black_felt.jpg",
+        import.meta.url
+      );
+      const blackFeltTexture = textureLoader.load(blackFelt.pathname);
+
+      const charcoal = new URL(
+        "./textures/snowman/charcoal.jpg",
+        import.meta.url
+      );
+      const charcoalTexture = textureLoader.load(charcoal.pathname);
+
+      const branch = new URL(
+        "./textures/snowman/branch_texture.jpg",
+        import.meta.url
+      );
+      const branchTexture = textureLoader.load(charcoal.pathname);
+
+      let snowmanObject: THREE.Object3D | null = null;
+
+      loader.load(
+        snowmanModel.pathname, // Replace with the path to your .obj file
+        (object: any) => {
+          // Called when the resource is loaded
+          object.scale.set(0.4, 0.4, 0.4);
+          object.position.set(-2, 1, 4);
+          object.traverse((child: any) => {
+            if (child.isMesh) {
+              if (child.name === "snowman_Sphere_Carrot") {
+                child.material.map = carrotTexture;
+              }
+              if (child.name === "hat_Cylinder.002_hat") {
+                child.material.map = blackFeltTexture;
+              }
+              if (child.name === "hat_Cylinder.002_hat_color") {
+                child.material.map = blackFeltTexture;
+              }
+              if (child.name === "snowman_Sphere_charcoal") {
+                child.material.map = charcoalTexture;
+              }
+              if (child.name === "charcoal_eyes_Sphere.004_charcoal") {
+                child.material.map = charcoalTexture;
+              }
+              // if ((child.name = "arms_Cylinder_branch")) {
+              //   child.material.map = branchTexture;
+              // }
+              //               snowman_Sphere_snow name
+              // page.tsx:145 snowman_Sphere_Carrot name
+              // page.tsx:145 snowman_Sphere_charcoal name
+              // page.tsx:145 arms_Cylinder_branch name
+              // page.tsx:145 charcoal_eyes_Sphere.004_charcoal name
+              // page.tsx:145 hat_Cylinder.002_hat name
+              // page.tsx:145 hat_Cylinder.002_hat_color name
+              // child.material.map = snowmanTexture; // Apply the texture to the material
+            }
+          });
+          scene.add(object);
+          snowmanObject = object;
+        },
+        (xhr: any) => {
+          // Called while loading is progressing
+          // console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+        },
+        (error: any) => {
+          // Called when loading has errors
+          console.error("An error happened", error);
+        }
+      );
+
+      // const snowmanGroup = new THREE.Group();
+      // scene.add(snowmanGroup);
+      // const snowmanGeometry = new THREE.SphereGeometry(1, 16, 16);
+      // const snowmanMaterial = new THREE.MeshStandardMaterial({
+      //   color: "white",
+      // });
+
+      // const snowman = new THREE.Mesh(snowmanGeometry, snowmanMaterial);
+      // snowman.scale.set(0.7, 0.7, 0.7);
+      // snowman.position.set(2.8, 0.3, 4.5);
+
+      // const snowmanhead = new THREE.Mesh(snowmanGeometry, snowmanMaterial);
+      // snowmanhead.scale.set(0.4, 0.4, 0.4);
+      // snowmanhead.position.set(2.8, 1.2, 4.5);
+
+      // snowmanGroup.add(snowman, snowmanhead);
 
       // Graves
-      const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2);
-      const graveMaterial = new THREE.MeshStandardMaterial();
+      const graveGeometry = new THREE.BoxGeometry(0.6, 0.6, 0.6);
+      const graveMaterial = new THREE.MeshStandardMaterial({ color: "red" });
 
       const graves = new THREE.Group();
       scene.add(graves);
@@ -195,15 +277,42 @@ function Page() {
       /**
        * Sky
        */
-      const sky = new Sky();
-      sky.scale.set(100, 100, 100);
-      scene.add(sky);
+      // const sky = new Sky();
+      // sky.scale.set(100, 100, 100);
+      // scene.add(sky);
 
-      sky.material.uniforms["turbidity"].value = 10;
-      sky.material.uniforms["rayleigh"].value = 5;
-      sky.material.uniforms["mieCoefficient"].value = 0.1;
-      sky.material.uniforms["mieDirectionalG"].value = 0.95;
-      sky.material.uniforms["sunPosition"].value.set(0.3, -0.05, -0.95);
+      // sky.material.uniforms["turbidity"].value = 10;
+      // sky.material.uniforms["rayleigh"].value = 5;
+      // sky.material.uniforms["mieCoefficient"].value = 0.1;
+      // sky.material.uniforms["mieDirectionalG"].value = 0.95;
+      // sky.material.uniforms["sunPosition"].value.set(-1, -0.02, -3);
+
+      // Geometry
+      const particlesGeometry = new THREE.BufferGeometry();
+      const count = 1000;
+
+      const positions = new Float32Array(count * 3); // Multiply by 3 because each position is composed of 3 values (x, y, z)
+
+      for (
+        let i = 0;
+        i < count * 3;
+        i++ // Multiply by 3 for same reason
+      ) {
+        positions[i] = (Math.random() - 0.5) * 10; // Math.random() - 0.5 to have a random value between -0.5 and +0.5
+      }
+
+      particlesGeometry.setAttribute(
+        "position",
+        new THREE.BufferAttribute(positions, 3)
+      ); // Create the Three.js BufferAttribute and specify that each information is composed of 3 values
+
+      const particlesMaterial = new THREE.PointsMaterial();
+      particlesMaterial.size = 0.03;
+      particlesMaterial.sizeAttenuation = true;
+
+      // Points
+      const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+      scene.add(particles);
 
       /**
        * Lights
@@ -218,15 +327,15 @@ function Page() {
       scene.add(directionalLight);
 
       // Cast and receive
-      directionalLight.castShadow = true;
-      ghost1.castShadow = true;
-      ghost2.castShadow = true;
-      ghost3.castShadow = true;
+      // directionalLight.castShadow = true;
+      // ghost1.castShadow = true;
+      // ghost2.castShadow = true;
+      // ghost3.castShadow = true;
 
-      walls.castShadow = true;
-      walls.receiveShadow = true;
-      roof.castShadow = true;
-      floor.receiveShadow = true;
+      // walls.castShadow = true;
+      // walls.receiveShadow = true;
+      // roof.castShadow = true;
+      // floor.receiveShadow = true;
 
       /**
        * Sizes
@@ -246,9 +355,9 @@ function Page() {
         0.1,
         100
       );
-      camera.position.x = 5;
-      camera.position.y = 5;
-      camera.position.z = 4;
+      camera.position.x = -2;
+      camera.position.y = 3;
+      camera.position.z = 8;
       scene.add(camera);
 
       // Controls
@@ -276,10 +385,13 @@ function Page() {
         const elapsedTime = timer.getElapsed();
 
         //jumping snowman
-        snowmanGroup.position.y = Math.sin(elapsedTime * 7) * 0.1 + 0.3;
+        // snowmanGroup.position.y = Math.sin(elapsedTime * 7) * 0.5 + 0.3;
+        if (snowmanObject) {
+          snowmanObject.position.y = Math.sin(elapsedTime * 5) * 0.3 + 0.3;
+        }
 
         // Ghosts
-        const ghost1Angle = elapsedTime * 5;
+        const ghost1Angle = elapsedTime * 15;
         ghost1.position.x = Math.cos(ghost1Angle) * 4;
         ghost1.position.z = Math.sin(ghost1Angle) * 4;
         ghost1.position.y =
@@ -287,7 +399,7 @@ function Page() {
           Math.sin(ghost1Angle * 2.34) *
           Math.sin(ghost1Angle * 3.45);
 
-        const ghost2Angle = -elapsedTime * 3;
+        const ghost2Angle = -elapsedTime * 10;
         ghost2.position.x = Math.cos(ghost2Angle) * 6;
         ghost2.position.z = Math.sin(ghost2Angle) * 6;
         ghost2.position.y =
@@ -295,7 +407,7 @@ function Page() {
           Math.sin(ghost2Angle * 2.34) *
           Math.sin(ghost2Angle * 3.45);
 
-        const ghost3Angle = elapsedTime * 3;
+        const ghost3Angle = elapsedTime * 30;
         ghost3.position.z = Math.sin(ghost3Angle) * 4;
         ghost3.position.y = ghost3.position.x = Math.cos(ghost3Angle);
         Math.sin(ghost2Angle) *
@@ -307,7 +419,7 @@ function Page() {
 
         // Render
         renderer.render(scene, camera);
-        renderer.shadowMap.enabled = true;
+        // renderer.shadowMap.enabled = true;
 
         // Call tick again on the next frame
         window.requestAnimationFrame(tick);
@@ -325,7 +437,7 @@ function Page() {
   return (
     <div className={styles.page}>
       <HomeButton />
-      <PageTitle title="haunted-house" />
+      <PageTitle title="haunted-house aka jumping snowman" />
       <canvas style={{ display: "block" }} ref={el}></canvas>;
     </div>
   );
